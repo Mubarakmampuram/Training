61517bb09043b3ddf4f8452fd34f7a726bacc8e7
Mubarakmampuram <110608671+Mubarakmampuram@users.noreply.github.com>
2022-08-09 00:48:22 +0530

    Update index.js
    parseintfn

--- task-7/index.js
+++ task-7/index.js
@@ -1,7 +1,9 @@
 let a,b,result;
 a = prompt("Enter first Number: ");
 b = prompt("Enter Second Number: ");
-let c = a + b ;
+let num1 = parseInt(a);
+let num2 = parseInt(b);
+let c = num1 + num2 ;
 
 result = (c>4) ? "over":"under" ;
 alert(result);
