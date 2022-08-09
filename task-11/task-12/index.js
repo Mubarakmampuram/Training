e8dc4c4b320c7bf27b3145b99ee86ac6f33ff1d8
mubarakmampuram <mubarak@neoito.com>
2022-08-09 14:26:43 +0530

    switch to if

---
+++ task-11/task-12/index.js
@@ -0,0 +1,10 @@
+let browser = prompt("which browser You Are Using?");
+if(browser=="edge"){
+    alert("You've Got The Edge..");
+}
+else if(browser=="chrome" || browser=="safari" || browser =="firefox" || browser =="opera"){
+    alert("Okay!,We Support These Browsers Too..");
+}
+else{
+    alert("We Hope That This Page Looks Okay!!..");
+}
