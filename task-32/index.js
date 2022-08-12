40b20ed91c8962ea0c222dc97aa9705c54268983
mubarakmampuram <mubarak@neoito.com>
2022-08-12 12:36:39 +0530

    if condn

--- task-32/index.js
+++ task-32/index.js
@@ -1,7 +1,10 @@
 function truncate(){
     let str = prompt("Enter Your Note Here");
     let maxlength = +prompt("what should be the length of your note?");
+    if(str.length > maxlength){
     let newstr = str.slice(0,maxlength) + "..." ;
-    alert(newstr);
+    alert(newstr);}else{
+        alert(str);
+    }
 }
 truncate();
